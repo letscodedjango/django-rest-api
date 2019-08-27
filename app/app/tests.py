f0a700f234d76ff6614b3eca2a878a237626d54b
Rajeshwar Sharma <rajeshwar.s@zymr.com>
2019-08-27 17:50:23 +0530

    Added setup config and autopep8

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,6 @@ class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that the function defined to add two numbers are working correctly or not"""
         self.assertTrue(add())
-
     def test_subtract_numbers(self):
         """ Test that function will subtract two numbers"""
         self.assertEqual(sub(20, 10), 10)
